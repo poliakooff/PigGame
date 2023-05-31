f37e22db82f348fbaac6f551dcd1a11406875465
Bohdan <redliz.redliz@gmail.com>
2023-05-31 11:28:58 +0300

    Added dice roll function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const btnRollDice = document.querySelector('.btn--roll');
+const dice = document.querySelector('.dice');
+
+// The function that roll the dice
+const rollDice = function () {
+  let randomDice = Math.trunc(Math.random() * 6) + 1;
+  dice.setAttribute('src', `dice${[randomDice]}.png`);
+};
+
+btnRollDice.addEventListener('click', rollDice);
